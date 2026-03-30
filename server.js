bd6dca5637db05dc24a0d5597454fb21da9b24bb
Harshitha <harshugr07@gmail.com>
2026-03-31 03:19:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const authRoutes = require('./auth');
+const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
